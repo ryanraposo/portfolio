6921a639680879a014c6edb3568d55430238e444
Ryan Raposo <raposo.ryan@gmail.com>
2022-10-03 13:18:47 -0400

    add conditional rendering and About section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import Navigation from './components/Navigation';
+import About from './components/About';
 
 function App() {
+
+  const [pages] = useState([
+    "about",
+    "portfolio",
+    "contact",
+    "resume"
+  ])
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
+  function renderCurrentPage() {
+    switch (currentPage) {
+      case pages[0]:
+        return (<About />);
+      case pages[1]:
+        return (<></>);
+      case pages[2]: // Contact
+        return (<></>);
+      case pages[3]: // Resume
+        return (<></>);
+      default:
+        return (<></>);
+    }
+  }
+  
   return (
     <div>
-      <Navigation />
+      <Navigation
+        pages={pages}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
+      <main>
+        {renderCurrentPage()}
+      </main>
     </div>
   );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,16 +3,22 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 
-function Navigation() {
+function Navigation(props) {
+  const {
+    pages,
+    setCurrentPage,
+    currentPage
+  } = props;
+  
   return (
     <Navbar bg="dark" variant="dark">
         <Container>
             <Navbar.Brand href="#home">Ryan Raposo</Navbar.Brand>
             <Nav className="me-auto">
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-                <Nav.Link href="#contact">Contact</Nav.Link>
-                <Nav.Link href="#resume">Resume</Nav.Link>
+                <Nav.Link href="#about" onClick={() => setCurrentPage(pages[0])}>About</Nav.Link>
+                <Nav.Link href="#portfolio" onClick={() => setCurrentPage(pages[1])}>Portfolio</Nav.Link>
+                <Nav.Link href="#contact" onClick={() => setCurrentPage(pages[2])}>Contact</Nav.Link>
+                <Nav.Link href="#resume" onClick={() => setCurrentPage(pages[3])}>Resume</Nav.Link>
             </Nav>
         </Container>
     </Navbar>
