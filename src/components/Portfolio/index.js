f7856f719294b3d72ff114c41e55c2159f75faaf
Ryan Raposo <raposo.ryan@gmail.com>
2022-10-03 14:13:35 -0400

    add Project component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,14 +1,48 @@
+import { useState } from 'react';
+import Project from '../Project';
+
+
 function Portfolio() {
+
+    const [projects] = useState([
+        {
+            name: "CodeUI", 
+            description: "Customize your color theme for VS Code",
+            image: "/assets/images/codeui-card-image.png",
+            link: "https://github.com/ryanraposo/codeui"
+        },
+        {
+            name: "Shard Projector", 
+            description: "Desktop dedicated servers for SteamCMD-based games",
+            image: "/assets/images/shardprojector-card-image.png",
+            link: "https://github.com/ryanraposo/shard-projector"
+        },
+        {
+            name: "PetHub",
+            description: "Share your pets",
+            image: "/assets/images/pethub-card-image.png",
+            link: "https://github.com/Beca-d/Project2"
+        },
+        {
+            name: "READirect",
+            description: "Pulls reviews on book-movie adaptations for easier comparison",
+            image: "/assets/images/readirect-card-image.png",
+            link: "https://github.com/katiebutler37/readirect"
+        }
+    ])
+    
     return (
         <div>
-            <ul>
-                <li>Project 1</li>
-                <li>Project 2</li>
-                <li>Project 3</li>
-                <li>Project 4</li>
-                <li>Project 5</li>
-                <li>Project 6</li>
-            </ul>
+                {projects.map((i) => {
+                    return (
+                        <Project
+                            name={i.name}
+                            description={i.description}
+                            image={i.image}
+                            link={i.link}
+                        />
+                    );
+                })}
         </div>
     );
 }
