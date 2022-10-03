112666490f4117440f9e5cb117cb2502a62f0519
Ryan Raposo <raposo.ryan@gmail.com>
2022-10-03 17:23:52 -0400

    add social links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,21 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="footer-copyright text-center py-3">
-                ©2022 Ryan Raposo
+                <a href="https://github.com/ryanraposo" className="github social">
+                    <svg width="30" height="30">       
+                        <image href="/assets/images/github.svg" height="30" width="30"/>
+                    </svg>
+                </a>
+                <a href="https://www.linkedin.com/in/ryan-raposo/" className="linkedin social">
+                    <svg width="30" height="30">       
+                        <image href="/assets/images/linkedin.svg" height="30" width="30"/>
+                    </svg>
+                </a>
+                <a href="https://stackoverflow.com/users/10915493/ryan-raposo" className="stackoverflow social">
+                    <svg width="30" height="30">       
+                        <image href="/assets/images/stackoverflow.svg" height="30" width="30"/>
+                    </svg>
+                </a>
             </div>
         </footer>
     )
