49889d11cd3dcacdd180587863bb83cdc3f7d038
Ryan Raposo <raposo.ryan@gmail.com>
2022-10-04 18:10:54 -0400

    add photo & bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,16 @@
 function About() {
     return (
-        <div>
-            <img src="../../img/longboy.jpg" alt="Ryan Raposo"></img>
-            <br/>
-            <p>Proident Lorem tempor veniam elit duis occaecat enim.</p>            
+        <div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
+            <div class="card p-4"> 
+                <div class=" image d-flex flex-column justify-content-center align-items-center">
+                    <img className="img-thumbnail" src={require("../../assets/images/portrait.jpg")} height="241" width="241asa" alt="Portrait" />
+                    <span class="name mt-3">Ryan Raposo</span>
+                    <span class="idd">Software Engineer</span>
+                    <div class="text mt-2">
+                        <span>Experienced creator & consultant from Toronto, Ontario. </span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
