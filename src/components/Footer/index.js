e568db1cb0846866dcda9075dc4d87a78502c432
Ryan Raposo <raposo.ryan@gmail.com>
2022-10-04 17:21:38 -0400

    fix footer svg pathing

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,17 +4,17 @@ function Footer() {
             <div className="footer-copyright text-center py-3">
                 <a href="https://github.com/ryanraposo" className="github social">
                     <svg width="30" height="30">       
-                        <image href="/assets/images/github.svg" height="30" width="30"/>
+                        <image href={require("../../assets/images/github.svg").default} height="30" width="30"/>
                     </svg>
                 </a>
                 <a href="https://www.linkedin.com/in/ryan-raposo/" className="linkedin social">
                     <svg width="30" height="30">       
-                        <image href="/assets/images/linkedin.svg" height="30" width="30"/>
+                        <image href={require("../../assets/images/linkedin.svg").default} height="30" width="30"/>
                     </svg>
                 </a>
                 <a href="https://stackoverflow.com/users/10915493/ryan-raposo" className="stackoverflow social">
                     <svg width="30" height="30">       
-                        <image href="/assets/images/stackoverflow.svg" height="30" width="30"/>
+                        <image href={require("../../assets/images/stackoverflow.svg").default} height="30" width="30"/>
                     </svg>
                 </a>
             </div>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,19 +6,18 @@ import Navbar from 'react-bootstrap/Navbar';
 function Navigation(props) {
   const {
     pages,
-    setCurrentPage,
-    currentPage
+    setCurrentPage
   } = props;
   
   return (
     <Navbar bg="dark" variant="dark">
         <Container>
-            <Navbar.Brand href="#home">Ryan Raposo</Navbar.Brand>
+            <Navbar.Brand>Ryan Raposo</Navbar.Brand>
             <Nav className="me-auto">
-                <Nav.Link href="#about" onClick={() => setCurrentPage(pages[0])}>About</Nav.Link>
-                <Nav.Link href="#portfolio" onClick={() => setCurrentPage(pages[1])}>Portfolio</Nav.Link>
-                <Nav.Link href="#contact" onClick={() => setCurrentPage(pages[2])}>Contact</Nav.Link>
-                <Nav.Link href="#resume" onClick={() => setCurrentPage(pages[3])}>Resume</Nav.Link>
+                <Nav.Link onClick={() => setCurrentPage(pages[0])}>About</Nav.Link>
+                <Nav.Link onClick={() => setCurrentPage(pages[1])}>Portfolio</Nav.Link>
+                <Nav.Link onClick={() => setCurrentPage(pages[2])}>Contact</Nav.Link>
+                <Nav.Link onClick={() => setCurrentPage(pages[3])}>Resume</Nav.Link>
             </Nav>
         </Container>
     </Navbar>
